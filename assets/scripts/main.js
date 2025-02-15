016e10f966b0dfbac179d84595a0f459f815dab9
Beafzzz <vadimgerenoggg@gmail.com>
2025-02-16 02:55:05 +0400

    чуть чуть доделал, но не до кинца

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,10 +5,17 @@ function appendForEquation(value){
     document.getElementById('display').value = equation
 }
 
+function deleteSymbol(){
+    const display = document.getElementById('display')
+    display.value = display.value.slice(0, -1)
+}
+
 function deleteAll(){
     document.getElementById('display').value = ""
+    equation = ''
 }
 
 function calculatedYO(){
     document.getElementById('display').value = eval(equation)
+    equation = ''
 }
